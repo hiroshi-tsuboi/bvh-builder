62dc18c49e3cd7f86d76d3cdb50de58af1e366c6
hiroshi-tsuboi <cy434483@jf7.so-net.ne.jp>
2023-06-27 07:09:33 +0900

    wip, load wavefront-obj

--- frontend/load-wavefront-obj.py
+++ frontend/load-wavefront-obj.py
@@ -1,6 +1,7 @@
 import pywavefront
 import socket
 import sys
+import struct
 
 filename = None
 
@@ -12,23 +13,55 @@ if filename is None:
     sys.exit(0)
 
 try:
-    scene = pywavefront.Wavefront(filename)
+    scene = pywavefront.Wavefront(filename, create_materials=True, collect_faces=True)
     print("success to load %s" % filename)
+    #print(scene.__dict__)
     for name, material in scene.materials.items():
         print(name)
-        print(material.vertex_format)
+        #print(material.vertex_format)
         #print(material.vertices)
-        print(material.diffuse)
-        print(material.ambient)
+        #print(material.diffuse)
+        #print(material.ambient)
+        #print(material.__dict__)
+        #print(material.has_faces)
+
+
+    for mesh in scene.mesh_list:
+        #print(mesh.__dict__)
+        for material in mesh.materials:
+            print(material.vertex_format)
+            pass
+        for face in mesh.faces:
+            pass        
 
 except:
     print("failed to load %s" % filename)
     sys.exit(-1)
 
-client6 = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+class Tcp():
+    def __init__(self):
+        self.client6_ = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 
-client6.connect(("::1", 8080))
+    def connect(self, address, port):
+        try:
+            self.client6_.connect((address, port))
+        except:
+            pass
 
-client6.send(b"Hello")
+    def sendString(self, string):
+        datas = bytearray()
+        datas.extend(map(ord, string))
+        self.client6_.send(datas)
 
-client6.close()
+    def close(self):
+        self.client6_.close()
+
+
+tcp = Tcp()
+
+tcp.connect("::1", 8080)
+
+tcp.sendString(filename)
+
+tcp.close()
+

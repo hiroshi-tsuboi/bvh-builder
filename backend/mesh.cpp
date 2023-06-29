79dde1eaa3fe5a29e0aee9dc6da2d5ac58ac8266
hiroshi-tsuboi <cy434483@jf7.so-net.ne.jp>
2023-06-29 12:06:46 +0900

    wip, load wavefront.obj

--- backend/mesh.cpp
+++ backend/mesh.cpp
@@ -1,7 +1,32 @@
 #include "mesh.h"
 
+#include <iostream>
+
 bool Mesh::load(Fifo* fifo)
 {
+	{
+		uint32_t materialCount;
+
+		fifo->read(&materialCount, sizeof(materialCount));
+
+		materials_.resize(materialCount);
+	}
+
+	for (auto& material: materials_)
+	{
+		fifo->read(material.name_);
+		std::cout << "name = " << material.name_ << std::endl;
+
+		fifo->read(material.vertexFormat_);
+		std::cout << "vertex format = " << material.vertexFormat_ << std::endl;
+
+		fifo->read(material.vertices_);
+		std::cout << "vertex count = " << material.vertices_.size() << std::endl;
+
+		// TODO
+	}
+
 	// TODO
+
 	return true;
 }

87a1bf2884dce4f017d946307c9f30e2b08afd3b
hiroshi-tsuboi <cy434483@jf7.so-net.ne.jp>
2023-07-01 18:00:37 +0900

    sort aabb by center

--- backend/divider.cpp
+++ backend/divider.cpp
@@ -1,9 +1,20 @@
 #include "divider.h"
 
+#include <utility>
+#include <algorithm>
 
 void Divider::process(std::vector<AaBb>& aabbs, uint32_t axisIndex)
 {
+	std::vector<std::pair<float, uint32_t> > centers;
+
+	centers.reserve(aabbs.size());
+
+	for (uint32_t i = 0; i < aabbs.size(); ++i)
+	{
+		centers.push_back(std::make_pair(aabbs.at(i).center(axisIndex), i));
+	}
+
+	std::sort(centers.begin(), centers.end());
 
 	// TODO
-
 }

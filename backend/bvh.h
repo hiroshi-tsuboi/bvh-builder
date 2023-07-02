f5dc93ed5dc876337d65b7c3aedc01b0c85c9f8f
hiroshi-tsuboi <cy434483@jf7.so-net.ne.jp>
2023-07-03 07:16:36 +0900

    wip, node leaf

--- backend/bvh.h
+++ backend/bvh.h
@@ -11,5 +11,32 @@ struct Bvh
 	std::mutex mutex_;
 	std::queue<std::thread*> queue_; 
 
+	enum
+	{
+		kNode,
+		kLeaf,
+	};
+
+	struct Obj
+	{
+		virtual int type() = 0;
+	};
+
+	struct Node : Obj
+	{
+		Obj* childs_[2];
+
+		int type() override { return kNode; } 
+	};
+
+	struct Leaf : Obj
+	{
+		std::vector<uint32_t> triangleIndices_;
+
+		int type() override { return kLeaf; } 
+	};
+
+	std::vector<Obj*> objs_;
+
 	bool build(const Triangular& triangular);
 };

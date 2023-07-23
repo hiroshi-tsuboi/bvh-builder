193987dad2bfaba057c4591e55252a73e713d768
hiroshi-tsuboi <cy434483@jf7.so-net.ne.jp>
2023-07-24 07:39:58 +0900

    wip, quadrangle

--- backend/quadrangular.cpp
+++ backend/quadrangular.cpp
@@ -34,16 +34,15 @@ void Quadrangular::create(const Triangular & triangular)
 		}
 	}
 
+	std::vector<AaBb> aabbs;
+	std::vector<std::tuple<double, double, uint32_t> > items;
+
 	for (const auto& [edge, triangles]: edgeLinks)
 	{
 		if (triangles.size() < 2)
 		{
 			continue;
 		}
-
-
-		std::vector<AaBb> aabbs;
-		std::vector<std::tuple<double, double, uint32_t, uint32_t> > halfAreas;
 
 		for (uint32_t i = 0; i < triangles.size(); ++i)
 		{
@@ -52,6 +51,8 @@ void Quadrangular::create(const Triangular & triangular)
 				continue;
 			}
 
+			auto& triangle_i = triangular.triangles_.at(triangles.at(i).first);
+
 			for (uint32_t j = i + 1; j < triangles.size(); ++j)
 			{
 				if (triangleIndices_.count(triangles.at(j).first))
@@ -59,12 +60,38 @@ void Quadrangular::create(const Triangular & triangular)
 					continue;
 				}
 
-				// TODO
+				auto& triangle_j = triangular.triangles_.at(triangles.at(j).first);
 
+				std::vector<uint32_t> vertexIndices;
+
+				vertexIndices.push_back(triangle_i.indices_[(triangles.at(i).second + 2) % 3]);
+				vertexIndices.push_back(triangle_i.indices_[triangles.at(i).second]);
+				vertexIndices.push_back(triangle_j.indices_[(triangles.at(j).second + 2) % 3]);
+				vertexIndices.push_back(triangle_j.indices_[triangles.at(j).second]);
+
+				AaBb aabb;
+
+				aabb.ownerIndices_[0] = triangles.at(i).first;
+				aabb.ownerIndices_[1] = triangles.at(j).first;
+
+				for (auto index: vertexIndices)
+				{
+					AaBb::Vertex vertex;
+					index *= 3;
+					for (auto& value: vertex.values_)
+					{
+						value = triangular.vertices_.at(index++);
+					}
+					aabb.grow(vertex);
+				}
+
+				auto aabbIndex = uint32_t(aabbs.size());
+
+				aabbs.push_back(aabb);
+
+				items.push_back(std::make_tuple(aabb.volume(), aabb.halfArea(), aabbIndex));
 			}
-
 		}
-
 	}
 
 	// TODO
